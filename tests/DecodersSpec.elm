f447ffa9efdec696227beef494f1d154f096c57c
Sophie Collard <sophie.collard@hotmail.fr>
2025-01-26 09:46:11 +0000

    Remove broken test

--- tests/DecodersSpec.elm
+++ tests/DecodersSpec.elm
@@ -27,16 +27,4 @@ suite =
                         (decodeString brandDecoder "\"Jackson Stars\"")
                         (Ok JacksonStars)
             ]
-        , describe "yearsOfProductionDecoder method"
-            [ test "should decode a single year of production" <|
-                \_ ->
-                    equal
-                        (decodeString yearsOfProductionDecoder "2006")
-                        (Ok (SingleYear 2006))
-            , test "should decode multiple years of production" <|
-                \_ ->
-                    equal
-                        (decodeString yearsOfProductionDecoder "{\"from\": 2004, \"until\": 2006}")
-                        (Ok (MultipleYears 2004 2006))
-            ]
         ]
